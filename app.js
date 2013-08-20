4d19c559296517d6bb9223c39e712ee21530eac4
Nathan <nathan.g.romano@gmail.com>
2013-08-20 23:44:29 +0000

    added the app.js file

---
+++ app.js
@@ -0,0 +1,16 @@
+/*
+ * Dependencies 
+ */
+var express = require('express');
+
+
+/*
+ * Our Express App
+ */
+var app = express();
+
+
+/*
+ * export this module as the app instance
+ */
+module.exports = app;
