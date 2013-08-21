a4ba235cbb91023d311fb596e02a1df80e7b049d
Nathan <nathan.g.romano@gmail.com>
2013-08-21 01:46:07 +0000

    renamed server to service

--- test/service.js
+++ test/service.js
@@ -1,8 +1,8 @@
 var service = require('../service');
 
-describe('given we have a server', function () {
+describe('given we have a service', function () {
 
-	describe('when the server is told to listen', function () {
+	describe('when the service is told to listen', function () {
 
 		it('should start', function (done) {
 			
