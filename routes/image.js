6d20e6df5f92b007e3ecf1f809b47ae31926f886
Nathan <nathan.g.romano@gmail.com>
2013-08-21 01:09:28 +0000

    added logic to validate the mime type of the image, and cleaned the code up a bit for readability

--- routes/image.js
+++ routes/image.js
@@ -24,12 +24,28 @@ var supportedTypes = ['image/x-png', 'image/pjpeg', 'image/jpeg', 'image/png', '
 
 router.post('/scale', function (req, res, next) {
 
-	var height = Math.abs(parseFloat(req.param('height')) || 1), width = Math.abs(parseFloat(req.param('width')) || 1);
+	var height = Math.abs(parseFloat(req.param('height')) || 1)
+		, width = Math.abs(parseFloat(req.param('width')) || 1)
+		, image = req.files.image;
 
-	if (!req.files.image) return res.set('content-type','text/plain').status(400).end(new Error('Need to submit an image to scale').toString());	
+	if (!image) 
+		return res	
+			.set('content-type','text/plain')
+			.status(400)
+			.end(new Error('Need to submit an image to scale').toString());	
+
+	if (supportedTypes.indexOf(image.type) < 0) 
+		return res
+			.set('content-type', 'text/plain')
+			.status(400)
+			.end(new Error('The image type is unsupported').toString());
 	
 	//TODO do it!
 	res.status(501).end('Not Implemented!');
+
+});
+
+router.post('*', function (req, res, next) {
 
 });
 
