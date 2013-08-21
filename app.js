5c11e5b7bb8d81f81d9d432eb82de17cf010fb99
Nathan <nathan.g.romano@gmail.com>
2013-08-21 00:05:54 +0000

    added basic configuration for our app

--- app.js
+++ app.js
@@ -11,6 +11,19 @@ var app = express();
 
 
 /*
+ * Configure our app
+ */
+
+app.configure(function () {
+	app.set('port', process.env.PORT || 3000);
+	app.use(express.logger('dev'));
+	app.use(express.bodyParser());
+	app.use(express.methodOverride());
+	app.use(app.router);
+});
+
+
+/*
  * export this module as the app instance
  */
 module.exports = app;
