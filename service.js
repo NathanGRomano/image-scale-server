7c845acd952c4d77cbb0a58e50bbeb49bfec52b6
Nathan <nathan.g.romano@gmail.com>
2013-08-21 01:59:13 +0000

    returns the output of the listen method

--- service.js
+++ service.js
@@ -5,7 +5,7 @@ var http = require('http')
 service.app = app;
 var _listen = service.listen;
 service.listen = function (cb) {
-	_listen.call(this, app.get('port'), function () {
+	return _listen.call(this, app.get('port'), function () {
 		console.log('image scale service listening on port ' + app.get('port') + ' in ' + app.get('env') + ' mode '); 
 		cb.apply(service, arguments);
 	});
